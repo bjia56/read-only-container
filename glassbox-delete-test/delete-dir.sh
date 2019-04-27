d30da919c0f360a6912c8d28af87f4d8b11477bc
Brett Jia <dev.bjia56@gmail.com>
2019-04-27 23:18:10 +0000

    minor modifications

--- glassbox-delete-test/delete-dir.sh
+++ glassbox-delete-test/delete-dir.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ -z "$1" ]; then
-    2>1 echo "need directory argument"
+    2>&1 echo "need directory argument"
     exit 1
 else
     DIR="$1"
 fi
-find $DIR -type f -exec rm -v {} \; > output.txt
+find $DIR -type f -exec rm -fv {} \; > output.txt 2>&1
